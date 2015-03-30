7e48f6e3514a19d097370a8477edda0ca34fc7c9
Carl Dawson <carl@invisible-gorilla.com>
2015-03-29 20:17:22 -0400

    Added files

---
+++ application/views/admin/groups.php
@@ -0,0 +1,15 @@
+<div id="sr-groups">
+	<section id="sr-create">
+		<h1>Create Groups</h1>
+
+		<form>
+			<input type="text" />
+
+			<button type="submit">Create</button>
+		</form>
+	</section><!-- #sr-create -->
+
+	<section id="sr-manage">
+
+	</section><!-- #sr-manage -->
+</div><!-- #sr-groups -->
